702e5219fe5a9c7db4c7e8d3e2ec474022aad72f
JoseRodrigues443 <josemiguel443@gmail.com>
2022-01-05 14:42:54 +0000

    added symlink

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -3,14 +3,19 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+import * as fs from 'fs';
+
 import { greeter } from "./deploy/greeter";
 import { job } from "./deploy/job";
 import { post } from "./deploy/post";
 
 async function main() {
-  await greeter("Hello, Hardhat!")
-  await post()
-  await job()
+  const addresses: { [id: string] : string; } = {};
+  addresses["greeter"] = await greeter("Hello, Hardhat!")
+  addresses["post"] = await post()
+  addresses["job"] = await job()
+
+  saveEnv(addresses)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -19,3 +24,19 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+function saveEnv(addresses: { [id: string]: string; }) {
+  const contractsDir = __dirname + "/../artifacts/contracts";
+
+  console.log(`Will save addresses in dir: ${contractsDir}`)
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify(addresses, undefined, 2)
+  );
+}
+
--- ui/src/client/ethereum/postClient.ts
+++ ui/src/client/ethereum/postClient.ts
@@ -1,8 +1,7 @@
 import { ethers } from 'ethers'
+
+// all the deployed contract addresses
+import addresses from './artifacts/contracts/contract-address.json'
+
+// Contract data
 import Post from './artifacts/contracts/Post.sol/Post.json'
-
-// Update with the contract address logged out to the CLI when it was deployed 
-const postContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-
-// Update with the contract address logged out to the CLI when it was deployed
-const jobContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
